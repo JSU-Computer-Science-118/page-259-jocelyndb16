5b3597a9b155b8cae56ab2a354fa2a08a5d77f06
jocelyndb16 <117838675+jocelyndb16@users.noreply.github.com>
2022-11-09 09:26:50 -0600

    259 assignment

--- main.cpp
+++ main.cpp
@@ -1,1 +1,38 @@
-// write your code here
+// Jocelyn Brown  J00966095  11/9/22
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	float a, b, c, x1, x2, discriminant, real, complex;
+	cout << "Enter coefficents a, b, and c: ";
+	cin >> a >> b >> c;
+	discriminant = b * b - 4 * a * c;
+
+	if (discriminant > 0)
+	{
+		x1 = (-b + sqrt(discriminant)) / (2 * a);
+		x2 = (-b - sqrt(discriminant)) / (2 * a);
+		cout << "Two real roots" << endl;
+		cout << "x1=" << x1<< endl;
+		cout << "x2= " << x2 << endl;
+
+	}
+
+	else if (discriminant==0)
+	{
+		cout << "the single root is:" << endl;
+		x1 = -b / (2 * a);
+		cout << "x1= x2=" << x1 << endl;
+
+	}
+	else
+	{
+		real = -b / (2 * a);
+		complex = sqrt(-discriminant) / (2 * a);
+			cout << "complex roots" << endl;
+			cin >> complex;
+	}
+	return 0;
+}
